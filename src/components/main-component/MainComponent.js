5868599dd40076a2d11c1461d6a42941b55adf75
Shanavas <shanavas@1cloudhub.com>
2022-09-29 18:17:51 +0530

    font change

--- src/components/main-component/MainComponent.js
+++ src/components/main-component/MainComponent.js
@@ -1,26 +1,19 @@
 import React from "react";
 import BannerComponent from "../banner-component/BannerComponent";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import SignIn from "../sign-in-component/signIn";
 function MainComponent() {
   return (
     <div className="main-content-height">
-  
-      <div> 
-      <Router> 
-      <Routes>
-        <Route exact path="/" element={<BannerComponent/>} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
-      </Router>
-       
-
-        </div>
+      <div>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<BannerComponent />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
