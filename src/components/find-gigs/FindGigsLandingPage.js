7576e0978c46dd092836415372cd3e84eb192628
Ajayprasad A <ajayprasad@1cloudhub.com>
2022-10-14 11:19:01 +0530

    added section 2 in find-gigs

--- src/components/find-gigs/FindGigsLandingPage.js
+++ src/components/find-gigs/FindGigsLandingPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../navbar-component/NavBar";
 import Section1 from "./sections/Section1";
+import Section2 from "./sections/Section2";
 
 function FindGigsLandingPage() {
   return (
     <div>
       <NavBar />
       <Section1 />
+      <Section2/>
     </div>
   );
 }
